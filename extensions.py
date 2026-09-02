3ec67a83462b4e8ee4c5b2d3ee94f988b8b90526
Riya <riyashrmaa04@gmail.com>
2026-09-02 12:54:43 +0530

    Add Redis for SocketIO

--- extensions.py
+++ extensions.py
@@ -1,7 +1,12 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_socketio import SocketIO
+import os
 
 db = SQLAlchemy()
 mail = Mail()
-socketio = SocketIO(async_mode="threading")
+
+socketio = SocketIO(
+    async_mode="threading",
+    message_queue=os.environ.get("REDIS_URL")
+)
